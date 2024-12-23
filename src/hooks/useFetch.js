f38ea138425bb128988cf44fa1fc5e6093a2101a
Salman-sajib <sfsajib@gmail.com>
2024-12-23 14:52:14 +0600

    built the use fetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -5,7 +5,36 @@ function useFetch(url, options) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  return <div>useFetch</div>;
+  useEffect(() => {
+    let isMounted = true; // To prevent state updates if component unmounts
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        const json = await response.json();
+        if (isMounted) {
+          setData(json);
+          setLoading(false);
+        }
+      } catch (error) {
+        if (isMounted) {
+          setError(error.message);
+          setLoading(false);
+        }
+      }
+    };
+    fetchData();
+
+    // clean up function
+    return () => {
+      isMounted = false;
+    };
+  }, [url, options]);
+
+  return { data, loading, error };
 }
 
 export default useFetch;
